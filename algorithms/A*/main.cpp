86a19447d141f9f6b36c9a73039bb10a519ff5fb
PedroPepeu <pedrosteamgtav@gmail.com>
2024-08-20 07:55:59 -0300

    A* algorithm

--- algorithms/A*/main.cpp
+++ algorithms/A*/main.cpp
@@ -2,8 +2,52 @@
 
 using namespace std;
 
+struct square {
+	int x;
+	int y;
+};
+
+// When D = D2 = 1 it is called Chebyshev distance
+float manhattan_dist(square node) {
+	// Where D is the minimal cost for moving
+	int dx = abs(node.x - goal.x);
+	int dy = abs(node.y - goal.y);
+	return D * (dx + dy);
+}
+
+float manhattan_dist_diag(square node) {
+	// Where D is the minimal cost for moving
+	// Where D2 is the minimal cost for moving in the diagonal
+	int dx = abs(node.x - goal.x);
+	int dy = abs(node.y - goal.y);
+	return D * (dx + dy) + (D2 - 2*D) * min(dx, dy);
+}
+
+float euclidean_dist(square node) {
+	int dx = abs(node.x - goal.x);
+	int dy = abs(node.y - goal.y);
+	return D * sqrt(dx*dx + dy*dy)
+}
+
+
 int A(vector<vector<int>> g) {
-	
+	set<pair<int, int>> f;
+	f.insert(start, 0);
+
+	int came_from = dict();
+	int cost_so_far = dict();
+	came_from[start] = None;
+	cost_so_far[start] = 0;
+
+	while(!f.empty()) {
+		pair<int, int> current = f.begin();
+
+		if(current == goal) {
+			break;
+		}
+
+		
+	}
 }
 
 int main() {
--- spoj/SUMFOUR/s.cpp
+++ spoj/SUMFOUR/s.cpp
@@ -1,39 +1,39 @@
-        #include <bits/stdc++.h>
+            #include <bits/stdc++.h>
 
-        using namespace std;
+            using namespace std;
 
-        int main() {
-            int n;
-            cin >> n;
+            int main() {
+                int n;
+                cin >> n;
 
-            int A[n], B[n], C[n], D[n];
+                int A[n], B[n], C[n], D[n];
 
-            for(int i = 0; i < n; i++) {
-                cin >> A[i] >> B[i] >> C[i] >> D[i];
-            }
-
-            int AB[n*n], CD[n*n];
-            int pos = 0;
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    AB[pos] = A[i]+B[j];
-                    CD[pos] = C[i]+D[j];
-                    pos++;
+                for(int i = 0; i < n; i++) {
+                    cin >> A[i] >> B[i] >> C[i] >> D[i];
                 }
-            }
-            sort(CD, CD+(n*n));
 
-            int count = 0;
-            for(int i = 0; i < n*n; i++) {
-                int posI = lower_bound(CD, CD+(n*n), -1*AB[i]) - CD;
-                if(posI != -1) {
-                    while(posI != n*n && CD[posI] == -1*AB[i]) {
-                        count++;
-                        posI++;
+                int AB[n*n], CD[n*n];
+                int pos = 0;
+                for(int i = 0; i < n; i++) {
+                    for(int j = 0; j < n; j++) {
+                        AB[pos] = A[i]+B[j];
+                        CD[pos] = C[i]+D[j];
+                        pos++;
                     }
                 }
-            }
+                sort(CD, CD+(n*n));
 
-            cout << count << endl;
-            return 0;
-        }
+                int count = 0;
+                for(int i = 0; i < n*n; i++) {
+                    int posI = lower_bound(CD, CD+(n*n), -1*AB[i]) - CD;
+                    if(posI != -1) {
+                        while(posI != n*n && CD[posI] == -1*AB[i]) {
+                            count++;
+                            posI++;
+                        }
+                    }
+                }
+
+                cout << count << endl;
+                return 0;
+            }
